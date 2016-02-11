d0b86603ed9df909449d606faafec8d3808e4fbb
Jac0bAnderson <jake912235@gmail.com>
2016-02-11 07:53:02 -0700

    C++ array
    Int array and double array.

---
+++ src/runner.cpp
@@ -0,0 +1,34 @@
+/*
+ * runner.cpp
+ *
+ *  Created on: Feb 9, 2016
+ *      Author: jand6944
+ */
+
+#ifndef SRC_RUNNER_CPP_
+#define SRC_RUNNER_CPP_
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int intArray[5] = {0, 1, 2, 3, 4};
+	for(int place = 0; place ==4; place++)
+	{
+	 cout << intArray[place] << endl;
+	}
+
+	double dubsArray[5] = {0.0, 1.0, 2.0, 3.0, 4.0};
+	for(int place = 0; place == 4; place++)
+	{
+		cout<< dubsArray[place] << endl;
+	}
+
+
+
+	return 0;
+}
+
+
+
+#endif /* SRC_RUNNER_CPP_ */
